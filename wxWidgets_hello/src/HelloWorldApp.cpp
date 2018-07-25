70b4066708be0445ac278f078521f8effc2989a5
young grasshopper <younggrasshopper@example.com>
2018-07-24 19:01:19 -0600

    Sublime Text project file, gitignore

--- wxWidgets_hello/src/HelloWorldApp.cpp
+++ wxWidgets_hello/src/HelloWorldApp.cpp
@@ -1,30 +1,70 @@
+// wxWidgets "Hello World" Program
+
 #include <iostream>
 
-// For compilers that don't support precompilation, include "wx/wx.h"
-#include "wx/wxprec.h"
-
+// For compilers that support precompilation, includes "wx/wx.h".
+#include <wx/wxprec.h>
 #ifndef WX_PRECOMP
-#	include "wx/wx.h"
+    #include <wx/wx.h>
 #endif
 
-#include "../include/hello_project/HelloWorldApp.h"
-#include "../include/hello_project/stuff/myclass.h"
-
-IMPLEMENT_APP(HelloWorldApp)
-
-// This is executed upon startup, like 'main()' in non-wxWidgets programs.
-bool HelloWorldApp::OnInit()
+class MyApp : public wxApp
 {
-	std::cout << "Hello, from HelloWorldApp." << std::endl;
-
-	MyClass mc;
-	mc.PrintTest();
-
-	wxFrame *frame = new wxFrame((wxFrame*) NULL, -1, _T("Hello wxWidgets World"));
-	frame->CreateStatusBar();
-	frame->SetStatusText(_T("Hello World"));
-	frame->Show(true);
-	SetTopWindow(frame);
-
-	return true;
+public:
+    virtual bool OnInit();
+};
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame();
+private:
+    void OnHello(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+};
+enum
+{
+    ID_Hello = 1
+};
+wxIMPLEMENT_APP(MyApp);
+bool MyApp::OnInit()
+{
+    std::cout << "Hello, world." << std::endl;
+    
+    MyFrame *frame = new MyFrame();
+    frame->Show(true);
+    return true;
+}
+MyFrame::MyFrame()
+    : wxFrame(NULL, wxID_ANY, "Hello World")
+{
+    wxMenu *menuFile = new wxMenu;
+    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
+                     "Help string shown in status bar for this menu item");
+    menuFile->AppendSeparator();
+    menuFile->Append(wxID_EXIT);
+    wxMenu *menuHelp = new wxMenu;
+    menuHelp->Append(wxID_ABOUT);
+    wxMenuBar *menuBar = new wxMenuBar;
+    menuBar->Append(menuFile, "&File");
+    menuBar->Append(menuHelp, "&Help");
+    SetMenuBar( menuBar );
+    CreateStatusBar();
+    SetStatusText("Welcome to wxWidgets!");
+    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
+    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
+    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+}
+void MyFrame::OnExit(wxCommandEvent& event)
+{
+    Close(true);
+}
+void MyFrame::OnAbout(wxCommandEvent& event)
+{
+    wxMessageBox("This is a wxWidgets Hello World example",
+                 "About Hello World", wxOK | wxICON_INFORMATION);
+}
+void MyFrame::OnHello(wxCommandEvent& event)
+{
+    wxLogMessage("Hello world from wxWidgets!");
 }
